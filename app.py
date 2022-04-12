fb2c007de6ca29424834d06dcc2534c4a13b76ac
Danny-Sibley <95097205+Danny-Sibley@users.noreply.github.com>
2022-04-12 08:59:54 -0700

    added freeze.py to build static site for deployment

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
@@ -12,5 +13,4 @@ def resume():
 
 
 if __name__ == '__main__':
-    # port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True)
+    app.run(debug=False)
